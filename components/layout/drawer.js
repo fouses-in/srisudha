51ba54e3fc50299183a803aecee41277d49207b0
manishkumar17963 <46640297+manishkumar17963@users.noreply.github.com>
2024-01-16 14:56:47 +0530

    adding drawer

--- components/layout/drawer.js
+++ components/layout/drawer.js
@@ -9,6 +9,10 @@ import Image from "next/image";
 import ChevronRightIcon from "../icons/outline/chevronRightIcon";
 import CheveronDownIcon from "../icons/outline/cheveronDownIcon";
 import { items } from "../../models/drawerItemModel";
+import { AnotherHeaderItem } from "./headerItem";
+import { MdOutlineWifiCalling3 } from "react-icons/md";
+import { IoMailUnreadOutline } from "react-icons/io5";
+import { FaChevronRight, FaChevronUp, FaRegAddressCard } from "react-icons/fa6";
 
 function Drawer({ isOpen, cancelhandler }) {
   const router = useRouter(null);
@@ -23,39 +27,34 @@ function Drawer({ isOpen, cancelhandler }) {
       onClick={cancelhandler}
     >
       <div
-        className={`duration-400 z-50 flex  h-full w-2/3 transform flex-col space-y-6 bg-white pb-4 transition-all duration-200 ease-in-out ${
+        className={`duration-400 z-50 flex  h-full w-2/3 max-w-xs transform flex-col space-y-6 bg-white pb-4 transition-all duration-200 ease-in-out ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
-        <div className="mx-3 mt-4 flex  items-center space-x-3 rounded-md border-b-1px  bg-profileCard py-2 px-2 text-gray-300">
-          <div className="relative h-8 w-8 rounded-full ">
+        <div className="no-scrollbar  flex w-full flex-1 flex-col overflow-scroll px-4 py-8 ">
+          <div
+            className={`relative flex-shrink-0 h-14 w-44  flex  flex-grow  justify-center overflow-hidden sm:flex-grow-0  md:flex-grow-0 `}
+          >
             <Image
-              src={`/images/profile.png`}
+              src={"/images/logo.png"}
+              alt="Sudha consultancy"
+              objectFit="contain"
               layout="fill"
-              objectFit="cover"
-              objectPosition="bottom"
-              className=" text-secondary shadow-lg"
             />
           </div>
-        </div>
-        <div className="no-scrollbar flex w-full flex-1 flex-col space-y-5 overflow-scroll px-2 py-0">
-          <div className="space-y-1">
+          <div className="space-y-1 mt-10">
             {items().map((value, index) => (
-              <div>
+              <div className="border-b-1px  py-1">
                 <div
                   key={value.label}
-                  className={`${
-                    router.pathname.startsWith(value.path)
-                      ? "bg-primary   rounded-l-lg border-l-2 border-primary bg-opacity-20 text-white"
-                      : ""
-                  }  ${
+                  className={` ${
                     value.expandable
                       ? ""
                       : "hover:text-white hover:bg-green-400"
-                  } flex gap-1.5 px-2 group py-2.5 rounded-md `}
+                  } flex gap-1.5 px-2 cursor-pointer group py-2.5 rounded-md `}
                   onClick={() => {
                     if (value.path) {
                       router.push(`${value.path}`);
@@ -68,12 +67,6 @@ function Drawer({ isOpen, cancelhandler }) {
                     }
                   }}
                 >
-                  <ChevronRightIcon
-                    className={`mr-2.5 h-4 text-gray-400 w-4  ${
-                      value.expandable ? "" : " group-hover:text-white"
-                    }`}
-                  />
-
                   <p
                     className={`text-sm ${
                       value.expandable ? "" : " group-hover:text-white"
@@ -81,16 +74,19 @@ function Drawer({ isOpen, cancelhandler }) {
                   >
                     {value.label}
                   </p>
-                  {value.expandable &&
-                    (selected == index ? (
-                      <ChevronRightIcon className="h-4 text-black self-center w-4" />
-                    ) : (
-                      <CheveronDownIcon className="h-4 text-black self-center w-4" />
-                    ))}
+                  {value.expandable && (
+                    <div className="p-1.5 border-1px rounded-sm">
+                      {selected == index ? (
+                        <FaChevronUp className="h-3 text-black self-center w-3" />
+                      ) : (
+                        <FaChevronRight className="h-3 text-black self-center w-3" />
+                      )}
+                    </div>
+                  )}
                 </div>
-                {value.childs?.length > 0 && (
+                {value.childs?.length > 0 && selected == index && (
                   <div
-                    className={`   ml-14  transition-all ease-out duration-300 rounded-md `}
+                    className={`  transition-all ease-out duration-300 rounded-md `}
                     style={{ height: selected == index ? "auto" : 0 }}
                   >
                     {value.childs?.map((value) => (
@@ -98,7 +94,7 @@ function Drawer({ isOpen, cancelhandler }) {
                         onClick={() => {
                           router.push(value.path);
                         }}
-                        className={`hover:text-white hover:bg-green-400 p-2  pl-3 rounded-md  text-sm font-normal  ${
+                        className={`hover:text-white cursor-pointer hover:bg-green-400 p-2  pl-4 rounded-md  text-sm font-normal  ${
                           router.pathname.startsWith(value.path)
                             ? "bg-primary   rounded-l-lg border-l-2 border-primary bg-opacity-20 text-white"
                             : ""
@@ -112,6 +108,27 @@ function Drawer({ isOpen, cancelhandler }) {
               </div>
             ))}
           </div>
+          <nav className="  text-xl gap-6 mt-10 font-medium  flex flex-col ">
+            {[
+              {
+                label: "Call us: +91 9843045936",
+                value: "(Mon-Sat)",
+                Icon: MdOutlineWifiCalling3,
+              },
+              {
+                label: "Mail us for help:",
+                value: "Sreenivasarao@asrfca.com",
+                Icon: IoMailUnreadOutline,
+              },
+              {
+                label: "2C, Thygraja Complex, Kilpauk",
+                value: "Chennai",
+                Icon: FaRegAddressCard,
+              },
+            ].map((value) => (
+              <AnotherHeaderItem {...value} />
+            ))}
+          </nav>
         </div>
       </div>
     </div>
--- models/drawerItemModel.js
+++ models/drawerItemModel.js
@@ -1,51 +1,33 @@
 export const items = () => [
   {
-    label: "Test Series",
+    label: "Services",
     expandable: false,
-    // path: '/test-series',
+    path: "/services",
   },
   {
-    label: "Course",
+    label: "About Us",
+    expandable: true,
+    childs: [
+      { label: "Our Team", link: "/team" },
 
+      { label: "Our Founder", link: "/ca-sreenivasa-rao" },
+
+      { label: "Testimonials", link: "/testimonials" },
+
+      { label: "Faqs", link: "/team" },
+
+      { label: "Contact Us", link: "/contact" },
+    ],
     // path: '/courses',
   },
   {
-    label: "Syllabus",
-
-    path: "/syllabus",
+    label: "Industry Compass",
   },
   {
-    label: "Free Resources",
+    label: "Portfolio",
+  },
+  {
+    label: "Resources",
     expandable: true,
-    // path: '/results',
-    childs: [
-      // { label: 'Notes', path: '/free-resources/notes' },
-      // { label: 'Books', path: '/free-resources/books' },
-      { label: "Previous Papers", path: "/free-resources/papers" },
-      { label: "Free Videos", path: "/free-resources/video" },
-    ],
-  },
-  {
-    label: "Achievements",
-    expandable: true,
-    // path: '/results',
-    childs: [
-      { label: "Results", path: "/results" },
-      { label: "Testimonials", path: "/testimonials" },
-      { label: "Placements", path: "/placements" },
-    ],
-  },
-
-  {
-    label: "Contact Us",
-    size: 4,
-  },
-  {
-    label: "About US",
-    size: 4,
-  },
-  {
-    path: "/privacy",
-    label: "Privacy Policy",
   },
 ];
